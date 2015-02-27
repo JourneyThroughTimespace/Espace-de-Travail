d823c97b946ed44ca950edee830354a89ce7e8fe
emaaix <blubber@hotmail.fr>
2015-02-27 17:31:51 +0100

    crédits fait
    aller et retour vers et depuis crédits actif

---
+++ Projet Unity/Assets/GoToCrédits.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToCrédits : MonoBehaviour {
+
+    public void NextLevelButton(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+}
---
+++ Projet Unity/Assets/retourmenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class retourmenu : MonoBehaviour {
+
+    public void NextLevelButton(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+	
+}
