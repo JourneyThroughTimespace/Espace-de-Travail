f967faeb8276c77c3338291dac84ba18f175e75f
emaaix <blubber@hotmail.fr>
2015-03-12 12:36:47 +0100

    menu add song

---
+++ Projet Unity/Assets/Assets Max/GameMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Awake();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        GameObject gameMusic = GameObject.Find("GameMusic");
+        GameObject gamemusic = GameObject.Find("Music");
+        if (gameMusic)
+        {
+            Destroy(gameMusic);
+            Destroy(gamemusic);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
